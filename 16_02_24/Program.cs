32de55df524e59e6d3ddc94f79ce56886e5cb8eb
alexandarcad01 <alexandaracad01@gmail.com>
2024-02-20 15:58:50 +0530

    all files has been added

---
+++ 16_02_24/Program.cs
@@ -0,0 +1,34 @@
+﻿//See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a=10;
+Console.WriteLine(a);
+Console.WriteLine("the given value for a is" +a);
+
+//int ,float , double ,string ,char ,long ,boolean ,array
+float b=26.5f;
+Console.WriteLine(b);
+Console.WriteLine("the given value for b is" +b);
+
+
+double c=10.5d;
+Console.WriteLine(c);
+Console.WriteLine("the given value for c is" +c);
+
+//check datatypes
+Console.WriteLine(c.GetType());
+
+char s='z';
+Console.WriteLine(s);
+Console.WriteLine("the given value for s is " + s);
+Console.WriteLine(s.GetType());
+
+string app="welcome to all";
+Console.WriteLine(app);
+Console.WriteLine("the given value for c is " + app);
+Console.WriteLine(app.GetType());
+
+
+//int getvalue;
+//getvalue=int.Parse(Console.ReadLine());
+//Console.WriteLine("getvalue");
+//Console.WriteLine("the given value through  console for getvalue  is " + getvalue);
