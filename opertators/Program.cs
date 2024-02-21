7908741feab0071233baa184ecf0771c4a99cbcb
alexandarcad01 <alexandaracad01@gmail.com>
2024-02-21 18:04:25 +0530

    all files has been added

---
+++ opertators/Program.cs
@@ -0,0 +1,53 @@
+﻿//Operators
+//Arithmetic Operators
+//1.Addition
+using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 5;
+      int b = 3;
+      Console.WriteLine(a + b);  
+     
+  //2.Subtraction
+
+      int c = 5;
+      int d = 3;
+      Console.WriteLine( - b);  
+     
+//3.Multiplication
+
+      int e = 15;
+      int f = 22;
+      Console.WriteLine(e * f);  
+     
+//4.Division
+  
+      int g = 12;
+      int h = 33;
+      Console.WriteLine(g / h);  
+    
+//5.Modulus
+
+      int i = 7;
+      int j = 3;
+      Console.WriteLine(i % j);  
+    
+//6.Increment
+
+      int k = 50;
+      k++;
+      Console.WriteLine(k);  
+     
+//7.Decrement
+
+      int l = 60;
+      l--;
+      Console.WriteLine(l);  
+     
+     }
+  }
+
+
